ec4488413741f76efa5e2609598c331f94b8240e
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-09-12 14:39:24 +0500

    Update.

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ImageGallery from '../Components/ImageGallery'
-
+import ImageGallery from './Components/ImageGallery'
 function App() {
 
   const images = [
